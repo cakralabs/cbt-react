17e3a31c261e97436009cb89bfc4f0826f3f8954
Cakra Aminuddin <cakra.amin@yahoo.com>
2021-03-14 16:49:44 +0700

    update login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -34,7 +34,44 @@ const Login = (props) =>{
     },[])
 
     return(       
-        <h1>Testing</h1>
+        <div>
+            <header className="masthead">
+                <div className="container-fluid">
+                    <div className="row no-gutters">
+                        <div className="col-md-12">
+                            <center><img src={ logo } width="100"/></center>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <div className="container-fluid">
+                <div className="main-content">
+                    <div className="container-fluid sm-width">
+                        <div className="row no-gutters">
+                            <div className="col-md-12">
+                                <div className="content">
+                                    <h1>Selamat datang,</h1>
+                                    <p>Silahkan login dengan username dan password yang telah anda miliki.</p>
+                                    <ConfigProvider form={{validateMessages}}>
+                                        <Form layout="vertical" onFinish={onFinish}>
+                                            <Item label="Email Address" validateTrigger="onBlur" name="email" rules={[{required: true,type:'email'}]}>
+                                                <Input autoComplete="off" />
+                                            </Item>
+                                            <Item label="Password" name="password" rules={[{required: true}]}>
+                                                <Input.Password autoComplete="off" />
+                                            </Item>
+                                            <div className="flex justify-between align-items-center mt-24">
+                                                <Button type="primary" htmlType="submit" loading={loading}>Log in</Button>
+                                            </div>
+                                        </Form>
+                                    </ConfigProvider>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
